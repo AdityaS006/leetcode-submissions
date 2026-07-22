05828ab7f41c916a91da37c4eb3982fbbbd5751d
AdityaS006 <adityasanadhaya@gmail.com>
2026-07-22 19:20:31 +0530

    Time: 0 ms (100.00%) | Memory: 20.4 MB (40.61%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -1,9 +1,20 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        n=len(nums)
-        target_left=target
-        for i in range(n):
-            for j in range(n):
-                if i!=j and nums[i]+nums[j]==target:
-                    return [i,j]
+        # Dictionary to store {number_we_have_seen: its_index}
+        seen_map = {} 
         
+        for i in range(len(nums)):
+            current_number = nums[i]
+            
+            # The exact number we need to find to hit the target
+            needed_number = target - current_number
+            
+            # If we've seen the needed number before, we found our pair!
+            if needed_number in seen_map:
+                # Return the index of the needed number, and our current index
+                return [seen_map[needed_number], i]
+            
+            # If not, log the current number and its index into the map for the future
+            seen_map[current_number] = i
+            
+        return []
